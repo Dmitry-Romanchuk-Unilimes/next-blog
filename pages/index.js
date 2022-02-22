b5d5af76ea9f38a18f23cc4b80c61316ac53cd88
Dmitry-Romanchuk-Unilimes <dmitry.romanchuk@unilimes.coms>
2022-02-22 11:45:35 +0200

    get blogs

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,15 @@ import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
 import CardListItem from 'components/CardListItem';
 import CardItem from 'components/CardItem';
+import { getAllBlogs } from 'lib/api';
 
-export default function Home() {
+export default function Home({ blogs }) {
 
   return (
     <PageLayout>
       <AuthorIntro />
       <hr />
+      {JSON.stringify(blogs)}
       <Row className="mb-5">
         <Col md="10">
           <CardListItem />
@@ -21,4 +23,13 @@ export default function Home() {
       </Row>
     </PageLayout>
   )
+}
+
+export async function getStaticProps() {
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs
+    }
+  }
 }
